b318226c7079e14e020fdb11cdaeb1f317f22164
Casey Burns <burnsc4@rpi.edu>
2018-07-08 23:55:32 -0700

    site overhaul, new layout

--- js/app.js
+++ js/app.js
@@ -11,20 +11,20 @@ function generateFadeTimes() {
 
 	var album_range    = [8000, 10000];
 	var title_range    = [5200, 7000];
-	var nav_range      = [5500, 7500];
+	// var nav_range      = [5500, 7500];
 	var preorder_range = [7500, 8500];
 
 	var album_time        = getRandomInRange(album_range);
 	var title_time        = getRandomInRange(title_range);
-	var nav_preorder_time = getRandomInRange(nav_range);
-	var nav_listen_time   = getRandomInRange(nav_range);
+	// var nav_preorder_time = getRandomInRange(nav_range);
+	// var nav_listen_time   = getRandomInRange(nav_range);
 	var preorder_time     = getRandomInRange(preorder_range);
 
 	var times = {
 		album: album_time,
 		title: title_time,
-		nav_preorder: nav_preorder_time,
-		nav_listen: nav_listen_time,
+		// nav_preorder: nav_preorder_time,
+		// nav_listen: nav_listen_time,
 		preorder: preorder_time
 	};
 
@@ -34,9 +34,9 @@ function generateFadeTimes() {
 function fadeInElements() {
 	var times = generateFadeTimes();
 	$('#album-img').fadeIn(times.album);
-	$('#site-title').fadeIn(times.title);
-	$('#preorder-nav').fadeIn(times.nav_preorder);
-	$('#listen-nav').fadeIn(times.nav_listen);
+	$('#navbar-header').fadeIn(times.title);
+	// $('#preorder-nav').fadeIn(times.nav_preorder);
+	// $('#listen-nav').fadeIn(times.nav_listen);
 	$('#preorder-container').fadeIn(times.preorder);
 };
 
